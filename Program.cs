af3490a4842ecfa93d7306c8dc3268c0e3c11156
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2023-01-23 22:30:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        product pro = new product();
+
+        pro.Id= 1;
+        pro.Name= "DEll Laptop";
+        pro.Price = 40000;
+
+        Console.WriteLine($"\n Id {pro.Id} \n Product Name : {pro.Name} \n  Product Price : {pro.Price}");
+
+        }
+    }
+}
---
+++ product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment9
+{
+    public class product 
+        
+        
+        
+        
+        // Auto implemented property:
+
+        /*  Property will work as a smart field.
+            We don't have to create variables to store & retrieve value 
+            Property will creates variable by its own.
+                        OR
+            Property will create backing fields */
+ 
+
+
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+    }
+
+}
